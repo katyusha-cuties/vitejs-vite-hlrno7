7c9bef0ade2434a0b8754042e155985df4f6e732
katyusha-cuties <katyusha.leader@icloud.com>
2022-06-09 17:01:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-hlrno7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-hlrno7)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img class="logo" alt="Vite logo" src="/vite.svg" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img class="logo" alt="Vue logo" src="./assets/vue.svg" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
